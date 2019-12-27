cba2b45c2ca30c98d90e5bf106f2f178ba47eedc
Alex <alexchenchen1@gmail.com>
2019-12-27 20:50:16 +0800

    Starting on recursion, added a helper function

--- DirectoryTree.py
+++ DirectoryTree.py
@@ -6,77 +6,85 @@ import pathlib
 import sys
 
 class Directory:
-    def __init__(self, path=None, strict_case=False):
-        self.directory = path
-        self.strict_case = strict_case
-        self.path = self.check_path(path)
+	def __init__(self, path=None, strict_case=False):
+		self.directory = path
+		self.strict_case = strict_case
+		self.path = self.check_path(path)
+	
+	def check_path(self, path):
+		# If we're on, in my case, a Mac, Python doesn't recognize the tilde expansion.
+		path = os.path.expanduser(path)
+		if os.path.exists(path):
+			return path
+		else:
+			print("Path does not exist. Defaulting to root of computer.") if path else pass
+			return self.goto_root()
 
-    def check_path(self, path):
-        # If we're on, in my case, a Mac, Python doesn't recognize the tilde expansion.
-        path = os.path.expanduser(path)
-        if os.path.exists(path):
-            return path
-        else:
-            print("Path does not exist. Defaulting to root of computer.") if path else pass
-            return self.goto_root()
-
-    def goto_root(self):
-        current = pathlib.Path(os.getcwd())
-        if current == current.parent:
-            return current
-        else:
-            while current.parent != current:
-                current = current.parent
-            return current
+	def goto_root(self):
+		current = pathlib.Path(os.getcwd())
+		if current == current.parent:
+			return current
+		else:
+			while current.parent != current:
+			current = current.parent
+			return current
 
 class DirectorySearcher(DirectoryTraverser): 
-    def __init__(self, target, path=None, strict_case=False):
-        Directory.__init__(self, path, strict_case)
-        self.target = target
+	def __init__(self, target, path=None, strict_case=False):
+		Directory.__init__(self, path, strict_case)
+		try:
+			self.target = Path(target)
+		except TypeError:
+			print("Unexpected type. Expecting string or similar type.")
 
-    def find_all(self, current=None, results=[]):
-        if not current:
-            current = self.path
-        
-        # TODO - if target is more than one layer, can we still find it?
-        current_items = os.listdir(current)
-        for item in current_items:
-            if Path(item) == Path(self.target):
-                results.append(os.path.abspath(Path(item)))
-            if 
+	def find_all(self, current=None):
+		"""
+		Traverses through entire directory tree by using Depth First Search, and appends all found results.
 
-        
+		Should basically accomplish the same functionality as pathlib.Path().glob
+		"""	
+		current = self.path
+		# TODO: remember pathlib.Path.iterdir is a thing, and can be used instead of an array
+				   
+		if self.target != self.target.name:
+			# TODO: does 'expanding', i.e. going to parents[1], parents[2], parents[4], parents[8], like a binary search, go faster? It should be O(log n)... right? 
+			#		as opposed to just doing a while loop
+			path_args = deque()
+			path_args.appendleft(self.target.name)
+			try:
+				while # is there an alternative to while True that works here?:
+					
+			
+			return self._r_path_find_all(current, [])
+		else:
+			return self._r_name_find_all(current, [])
+			# Base name is the same, but is the rest of it same?
+			# Also - do .lnks, junctions, symbolic links etc. work fine?
+
+				
 
 def help():
-    print('''
+	print('''
 Directory Explorer by Alex Chen
 Accepts relative paths, absolute paths (relative paths take priority), and all arguments and options are case insensitive.
 
-    Arguments:
-        --d
-            Displays directories and files in current directory. Display depth can be configured with options (see below). 
-            If --path is specified, displays under path instead, essentially the same as going into the directory specified with --path, calling display, and then coming back to current working directory.
-        
-        --s [file or directory name]
-            Searches directories for all occurences of specified file. Search depth can be configured with options (see below).
-            In the future: implement regex support for this.
+	Arguments:
+		--d
+			Displays directories and files in current directory. Display depth can be configured with options (see below). 
+			If --path is specified, displays under path instead, essentially the same as going into the directory specified with --path, calling display, and then coming back to current working directory.
+		
+		--s [file or directory name]
+			Searches directories for all occurences of specified file. Search depth can be configured with options (see below).
+			In the future: implement regex support for this.
 
-        --f [file or directory name]
-            Similar to --s, but will return only the first found occurence. Typically finds the \'shallowest\' result. 
-    
-    Options:
-        --path [path] 
-            Specifies which directory to execute under. Default is current directory.
-        
-        --strict-case
-            Will perform case-sensitive Search.
+		--f [file or directory name]
+			Similar to --s, but will return only the first found occurence. Typically finds the \'shallowest\' result. 
+				
+	Options:
+		--path [path] 
+			Specifies which directory to execute under. Default is current directory.
+				
+		--strict-case
+			Will perform case-sensitive Search.
 ''')
 
-
-if __name__ == '__main__':
-    test = {'help': help} # To add: display, search, etc.
-    args = sys.argv[2:]
-    try:
-        test[sys.argv[1]](*args)
-    except (ValueError, TypeError):
-        help()
