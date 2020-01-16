9ac0f4128d064a4478239b6a0866a8fe7c6ea7a2
Alex <alexchenchen1@gmail.com>
2020-01-16 01:43:14 -0500

    Modified some Django files in preparation of fiddlin with REST API stuff. (Random fiddling with Json)

--- Testing/JsonTest.py
+++ Testing/JsonTest.py
@@ -7,7 +7,21 @@ for entry in json_dict:
 	print(entry)
 
 print("\n")
-print(type(json_dict["players"][0]))
-for x in json_dict["players"][0]:
-	print(x)
-	print(type(json_dict["players"][0][x]))
+print(type(json_dict["championPatch"][0]))
+patch_champs = {}
+
+for x in json_dict["championPatch"]:
+	try:
+		patch_champs[x["rank"]].append(x["champion"])
+	except KeyError:
+		patch_champs[x["rank"]] = [x["champion"]]
+
+counter = 0
+for x in range(0, 200):
+	try:
+		for y in patch_champs[x]:
+			counter += 1
+			print(x, y)
+	except KeyError:
+		pass
+print(len(json_dict["championPatch"]), counter)
