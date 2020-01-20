4bb04b80de525510f6d2641cc3f498cc1bfc7ce8
Alex <alexchenchen1@gmail.com>
2020-01-20 07:57:24 -0500

    Trying out the QuickStart guide from Django REST API

--- Testing/TestDjango/TestDjango/asgi.py
+++ Testing/TestDjango/TestDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ServerDirectoryList project.
+ASGI config for TestDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ServerDirectoryList.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDjango.settings')
 
 application = get_asgi_application()
